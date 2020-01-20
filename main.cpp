95ff926b03f01fb0d1e3a2ad54166f9edc5b7805
discohead <jaredcolin@gmail.com>
2020-01-20 22:50:01 +0000

    add examples

--- main.cpp
+++ main.cpp
@@ -51,42 +51,182 @@ struct CarWash            //1) a U.D.T. with a random number of member variables
 /*
  1)
  */
+ struct Oscillator
+ {
+   float phase = 0.0;
+   float amplitude = 0.0;
+   unsigned int waveType = 0;
+
+   void init(float p, float a, unsigned int w);
+   void play();
+   void stop();   
+ }
 
 /*
  2)
  */
+ struct Envelope
+ {
+   float attack = 0.1;
+   float decay = 0.1;
+   float sustain = 1.0
+   float release = 0.5;
+
+   void init(float a, float d, float s, float r)
+   void trigger();
+   void reset();
+ }
 
 /*
  3)
  */
+ struct Lamp
+ {
+   bool on = false;
+
+   struct Bulb
+   {
+     float brightness = 100.0
+     float warmth = 0.5;
+   }
+
+   void changeBulb(Bulb bulb);
+   void flipSwitch(on);
+ }
+
 
 /*
  4)
  */
+ struct Book
+ {
+   unsigned int numPages = 100;
+   unsigned int currentPage = 0;
+   float fontSize = 12.0;
+   bool isHardBack = false;
+
+   void turnPage();
+ }
 
 /*
  5)
  */
+ struct Cycle
+ {
+   unsigned int numWheels = 2;
+   
+   struct Wheel
+   {
+     float diameter = 2.0;
+     int numSpokes = 20;
+   }
+
+   void addWheel(Wheel wheel);
+   void pedal();
+   void brake();
+ }
 
 /*
  6)
  */
+ struct Machine
+ {
+   float voltage = 10.0
+   unsigned int numWidgets = 100;
+
+   struct Widget
+   {
+     float rotatorSize = 1.0;
+     unsigned int shape = 1;
+   }
+
+   struct Foo
+   {
+     float bar = 0.0;
+   }
+
+   void addWidget(Widget w);
+   float processFoo(Foo foo);
+ }
 
 /*
  7)
  */
+ struct Phone
+ {
+   float screenSize = 5.7 ;
+   bool isSmart = true;
+   
+   void makeCall(int phoneNumber);
+ }
 
 /*
  8)
  */
+struct Keyboard
+{
+  bool isQwerty = true;
+  bool hasNumpad = false;
+  char currentKeyDepressed;
 
+  void setCurrentKeyDepressed(char key);
+
+}
 /*
  9)
  */
+ struct Can
+ {
+   float height = 5.0;
+   float radius = 2.0;
+   bool isOpen = false;
 
+   void open();
+   float getVolume();
+ }
 /*
  10)
  */
+ struct Synthesizer
+ {
+  struct Oscillator
+  {
+    float phase = 0.0;
+    float amplitude = 0.0;
+    unsigned int waveType = 0;
+
+    void init(float p, float a, unsigned int w);
+    void play();
+    void stop();   
+  }
+
+  struct Envelope
+  {
+    float attack = 0.1;
+    float decay = 0.1;
+    float sustain = 1.0
+    float release = 0.5;
+
+    void init(float a, float d, float s, float r)
+    void trigger();
+    void reset();
+  }
+
+  struct Filter
+  {
+    float cutoff = 600.0;
+    float resonance = 0;
+    int numPoles = 12;
+  }
+
+  Oscillator osc;
+  Envelope filterEnv;
+  Envelope ampEnv;
+  Filter filter;
+
+  void init(Oscillator osc, Filter filter, Envelope filterEnv, Envelope ampEnv);
+  float render(float ampLevel);
+ }
 
 #include <iostream>
 int main()
